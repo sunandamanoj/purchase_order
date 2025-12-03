bbff51eb440ff9d7a2b1623e97fd3bb48ebb7dab
tmgomas <75267948+tmgomas@users.noreply.github.com>
2025-12-04 00:35:12 +0530

    Purchase

--- admin/purchase_orders/index.php
+++ admin/purchase_orders/index.php
@@ -5,7 +5,7 @@
 <?php endif;?>
 <div class="card card-outline card-primary">
 	<div class="card-header">
-		<h3 class="card-title">List of Purchase Orders & Quotation</h3>
+		<h3 class="card-title">List of Purchase Purchase Orders & Quotation</h3>
 		<div class="card-tools">
 			<a href="?page=purchase_orders/manage_quotation" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New Quotation</a>
 
